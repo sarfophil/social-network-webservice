e1d3a3887744a2f3592bff6aa772fb912a66a57d
Tesfai Gebrekidan <tesfaigebrekidan@Tesfai.local>
2020-04-01 00:55:02 -0500

    commited by tesfai

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({"err":"happy"});
+});
+
 module.exports = router;
