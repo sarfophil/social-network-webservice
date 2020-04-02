e5734cb0a9511499692f4a2cf20324e93c5534e0
sarfophil <philsarfogh@gmail.com>
2020-04-01 21:35:01 -0500

    Add Search Engine

--- routes/post-route.js
+++ routes/post-route.js
@@ -12,7 +12,6 @@
 router.get('/search',function(req,res) {
     let username = req.query.query;
     let limit = parseInt(req.query.limit)
-    console.log(`${username} - ${limit}`)
     searchService.search(username,limit,(err,doc) => {
        // console.log(doc)
         res.status(200).send(doc)
