729d937789403617d1ab09721d7abdb3c2b12936
sarfophil <philsarfogh@gmail.com>
2020-04-03 21:32:53 -0500

    add  Blacklisting Module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const postAdvertisement = require('./post-route');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/admin/ads', postAdvertisement.postAdvertisement);
+
+
+
+
 
 
 module.exports = router;
