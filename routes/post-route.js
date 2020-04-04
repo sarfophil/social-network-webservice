42ae984fd627725f9cfc4a82f49fd2ad0358c08f
sarfophil <philsarfogh@gmail.com>
2020-04-04 11:42:17 -0500

    Add Authentication and Authorization

--- routes/post-route.js
+++ routes/post-route.js
@@ -9,11 +9,29 @@ const fileStorageService = require('../service/filestorage-service')
 const postService = require('../service/post-service')
 const searchService = require('../service/search-service')
 
-// util
-// const Utils = require('../util/appUtil') 
+const bcrypt = require('../util/bcrypt')
+const jwt = require('../util/jwt')
 
 
-
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    UserModel.findOne({$or : [{username: {$eq: username}},{email: {$eq: username}}]},function (err,user) {
+      if(err) res.statusCode(403)
+      let comparePassword = bcrypt.compareSync(password,user.password) 
+      if(comparePassword){
+        jwt.sign(user,(err,token) => {
+           if(err) {
+              res.status(500).send('Unable to sign token')
+           }else{ 
+             res.status(200).send({access_token: token})
+           }
+        })
+      }else{
+         res.sendStatus(403)
+      }
+    })
+  });
 
 // search post
 router.get('/search',function(req,res) {
